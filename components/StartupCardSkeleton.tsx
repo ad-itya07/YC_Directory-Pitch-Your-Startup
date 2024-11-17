8f513433edfe1a4f5620ad237b5abde11d4da98a
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-17 14:57:13 +0530

    card skeleton while loading

---
+++ components/StartupCardSkeleton.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/lib/utils";
+import { Skeleton } from "./ui/skeleton";
+
+const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => (
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>
+);
+
+export default StartupCardSkeleton;
