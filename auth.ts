d34bcec053a08248d3d5faee90d2868fa7b81732
ad_itya07 <jhaaditya2207@gmail.com>
2024-11-17 15:02:05 +0530

    session generation

--- auth.ts
+++ auth.ts
@@ -1,6 +1,61 @@
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+import { profile } from "console";
+
+let userId : string;
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
-})
+  callbacks: {
+    async signIn({
+      user: { name, email, image },
+      profile: { id, login, bio },
+    }) {
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+        console.log(id);
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+
+      userId = id;
+      return true;
+    },
+
+    async jwt({ token, account, profile }) {
+      // console.log(token);
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
+        // token.id = user?.id;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      // console.log(session);
+      Object.assign(session, { id: token._id});
+      // console.log(session);
+      return session;
+
+    },
+  },
+});
